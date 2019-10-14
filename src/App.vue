24de342834457a8c4e55407dff29faeed1856258
Jia-Yin <jywglady@gmail.com>
2019-10-14 22:09:30 +0800

    Sin Animation

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="startsin">start</button>&nbsp;&nbsp;
+    <button @click="stopsin">stop</button>
+    <graph-line
+            :width="600"
+            :height="400"
+            :shape="'normal'"
+            :axis-min="-1.5"
+            :axis-max="1.5"
+            :axis-full-mode="true"
+            :labels="mylabels"
+            :names="names"
+            :values="values">
+        <note :text="'Line Chart'"></note>
+        <tooltip :names="names" :position="'right'"></tooltip>
+        <legends :names="names"></legends>
+        <guideline :tooltip-y="true"></guideline>
+    </graph-line>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'app',  
+  data() {
+    return {
+      names: [ 'Data' ],
+      values: [
+        [  ]
+      ],
+      mylabels: [ 0 ],
+      idx: 0,
+      timer: null
+    }
+  },
+  methods: {
+    startsin: function() {
+      this.timer = setInterval(this.addData, 100)
+    },
+    stopsin: function() {
+      clearInterval(this.timer)
+    },
+    addData: function() {
+      this.values[0].push(Math.sin(2*3.14159*this.idx/20))
+      this.idx++
+      this.mylabels.push(this.idx)
+      if (this.values[0].length > 20) {
+        this.values[0].shift()
+        this.mylabels.shift()
+      }
+    },
+  },
+
 }
 </script>
 
